d2c03c17fbc51a1c71f7d82d1b6967b7127bcf9c
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 15:58:38 +0100

    Adds entity representing customer meetings

--- src/main/java/app/estateagency/jpa/entities/CustomerMeeting.java
+++ src/main/java/app/estateagency/jpa/entities/CustomerMeeting.java
@@ -1,2 +1,34 @@
-package app.estateagency.jpa.entities;public class CustomerMeeting {
+package app.estateagency.jpa.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
+@Entity
+@Table(name = "customer_meeting", schema = "real_estate")
+@Data
+public class CustomerMeeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
+
+    private LocalDateTime date;
 }

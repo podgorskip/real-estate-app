3eb06b478ddadd579e69dc24ca2c06a9ade9b04c
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:16:00 +0100

    Adds entity representing estate

--- src/main/java/app/estateagency/jpa/entities/Estate.java
+++ src/main/java/app/estateagency/jpa/entities/Estate.java
@@ -1,2 +1,70 @@
-package app.estateagency.jpa.entities;public class Estate {
+package app.estateagency.jpa.entities;
+
+import app.estateagency.enums.estate.Availability;
+import app.estateagency.enums.estate.Condition;
+import app.estateagency.enums.credit.EstateType;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "estate", schema = "real_estate")
+@Data
+public class Estate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
+
+    @Enumerated(value = EnumType.STRING)
+    private EstateType type;
+
+    private int bathrooms;
+
+    private int rooms;
+
+    private boolean garage;
+
+    private int storey;
+
+    private String location;
+
+    private boolean balcony;
+
+    private String description;
+
+    private Availability availability;
+
+    private double size;
+
+    private Condition condition;
+
+    private double offeredPrice;
+
+    private LocalDateTime postDate;
+
+    @OneToOne(mappedBy = "estate", cascade = CascadeType.ALL)
+    private Offer offer;
+
+    @OneToMany(mappedBy = "estate", cascade = CascadeType.ALL)
+    private Set<Photo> photos;
+
+    @OneToMany(mappedBy = "estate", cascade = CascadeType.ALL)
+    private Set<Document> documents;
+
+    @OneToOne(mappedBy = "estate", cascade = CascadeType.ALL)
+    private HistoryOffer historyOffer;
+
+    @PrePersist
+    private void prePersist() {
+        postDate = LocalDateTime.now();
+    }
 }

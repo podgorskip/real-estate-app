10750ac0f17b310c281b10cf7d4847dc38293ba6
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 15:53:29 +0100

    Adds enum representing emploment status for credit worthiness

--- src/main/java/app/estateagency/enums/credit/EmploymentStatus.java
+++ src/main/java/app/estateagency/enums/credit/EmploymentStatus.java
@@ -1,4 +1,4 @@
-package app.estateagency.enums;
+package app.estateagency.enums.credit;
 
 public enum EmploymentStatus {
     EMPLOYED, UNEMPLOYED, SELF_EMPLOYED, RETIRED, STUDENT, HOMEMAKER, ON_LEAVE

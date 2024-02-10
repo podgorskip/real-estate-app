6028c6e664909e9384079108dfaa2e2668992b5d
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:20:05 +0100

    Adds entity representing estate photo

--- src/main/java/app/estateagency/jpa/entities/Photo.java
+++ src/main/java/app/estateagency/jpa/entities/Photo.java
@@ -1,2 +1,31 @@
-package app.estateagency.jpa.entities;public class Photo {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "photo", schema = "real_estate")
+@Data
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "estate_id")
+    private Estate estate;
+
+    private String filename;
+
+    private String contentType;
+
+    private int fileSize;
+
+    private LocalDateTime uploadDate;
+
+    @PrePersist
+    private void prePersist() {
+        uploadDate = LocalDateTime.now();
+    }
 }

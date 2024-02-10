ef4b3e375ba51c25774f9993e23b1495fec236b1
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 15:58:15 +0100

    Adds entity representing customer user

--- src/main/java/app/estateagency/jpa/entities/Customer.java
+++ src/main/java/app/estateagency/jpa/entities/Customer.java
@@ -1,2 +1,33 @@
-package app.estateagency.jpa.entities;public class Customer {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Table(name = "customer", schema = "real_estate")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private Set<CustomerMeeting> meetings;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private Set<OfferVisit> offerVisits;
+
+    @ManyToMany(mappedBy = "customers")
+    private Set<Offer> likes;
+
+    @ManyToMany(mappedBy = "customers")
+    private Set<Preference> preferences;
+
+    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL)
+    private CreditWorthinessInfo creditWorthinessInfo;
 }

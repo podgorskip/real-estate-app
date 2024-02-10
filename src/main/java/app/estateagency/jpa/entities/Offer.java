ca168ae2deceb3a382af950fddfe619fd82513a0
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:18:51 +0100

    Adds entity representing offer

--- src/main/java/app/estateagency/jpa/entities/Offer.java
+++ src/main/java/app/estateagency/jpa/entities/Offer.java
@@ -1,2 +1,47 @@
-package app.estateagency.jpa.entities;public class Offer {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "offer", schema = "real_estate")
+@Data
+public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "estate_id")
+    private Estate estate;
+
+    private double price;
+
+    private String description;
+
+    private LocalDateTime postDate;
+
+    @OneToMany(mappedBy = "offer", cascade = CascadeType.ALL)
+    private Set<OwnerMeeting> ownerMeetings;
+
+    @OneToMany(mappedBy = "offer", cascade = CascadeType.ALL)
+    private Set<CustomerMeeting> customerMeetings;
+
+    @OneToMany(mappedBy = "offer", cascade = CascadeType.ALL)
+    private Set<OfferVisit> offerVisits;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "likes",
+            joinColumns = @JoinColumn(name = "offer_id"),
+            inverseJoinColumns = @JoinColumn(name = "customer_id")
+    )
+    private Set<Customer> customers;
+
+    @PrePersist
+    private void prePersist() {
+        postDate = LocalDateTime.now();
+    }
 }

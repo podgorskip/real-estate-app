38574f3432d63001d9920749eb99026a9826fcfd
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:19:41 +0100

    Adds entity representing owner meetings

--- src/main/java/app/estateagency/jpa/entities/OwnerMeeting.java
+++ src/main/java/app/estateagency/jpa/entities/OwnerMeeting.java
@@ -1,2 +1,34 @@
-package app.estateagency.jpa.entities;public class OwnerMeeting {
+package app.estateagency.jpa.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
+@Entity
+@Table(name = "owner_meeting", schema = "real_estate")
+@Data
+public class OwnerMeeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    private LocalDateTime date;
 }

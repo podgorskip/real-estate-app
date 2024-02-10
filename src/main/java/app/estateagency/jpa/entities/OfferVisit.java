d73a29c90ce8194643b6f3d1d2e6161b4bba2a57
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:19:19 +0100

    Adds entity representing estate views

--- src/main/java/app/estateagency/jpa/entities/OfferVisit.java
+++ src/main/java/app/estateagency/jpa/entities/OfferVisit.java
@@ -1,2 +1,24 @@
-package app.estateagency.jpa.entities;public class OfferVisit {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "offer_visits", schema = "real_estate")
+@Data
+public class OfferVisit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    private LocalDateTime date;
 }

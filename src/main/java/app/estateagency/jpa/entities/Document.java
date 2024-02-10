a8f3d76db740570091cfdde2bcef5a3af6627b37
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:15:44 +0100

    Adds entity representing document

--- src/main/java/app/estateagency/jpa/entities/Document.java
+++ src/main/java/app/estateagency/jpa/entities/Document.java
@@ -1,2 +1,29 @@
-package app.estateagency.jpa.entities;public class Document {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "document", schema = "real_estate")
+@Data
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "estate_id")
+    private Estate estate;
+
+    private String filename;
+
+    private String description;
+
+    private LocalDateTime uploadDate;
+
+    @PrePersist
+    private void prePersist() {
+        uploadDate = LocalDateTime.now();
+    }
 }

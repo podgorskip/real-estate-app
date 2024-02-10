7a78bb4506c99511ee26aa120094545f6d14603f
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 15:56:37 +0100

    Adds entity representing agent user

--- src/main/java/app/estateagency/jpa/entities/Agent.java
+++ src/main/java/app/estateagency/jpa/entities/Agent.java
@@ -1,2 +1,30 @@
-package app.estateagency.jpa.entities;public class Agent {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Table(name = "agent", schema = "real_estate")
+@Data
+public class Agent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "agent")
+    private Set<Calendar> calendars;
+
+    @OneToMany(mappedBy = "agent")
+    private Set<Estate> estates;
+
+    @OneToMany(mappedBy = "agent")
+    private Set<OwnerMeeting> ownerMeetings;
+
+    @OneToMany(mappedBy = "agent")
+    private Set<CustomerMeeting> customerMeetings;
 }

aceb98397ea3a4c1ca2e6290e2cca0f55c1ac2c0
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:20:29 +0100

    Adds entity representing customers preference

--- src/main/java/app/estateagency/jpa/entities/Preference.java
+++ src/main/java/app/estateagency/jpa/entities/Preference.java
@@ -1,2 +1,24 @@
-package app.estateagency.jpa.entities;public class Preference {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Table(name = "preference", schema = "real_estate")
+@Data
+public class Preference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "preference_customer",
+            joinColumns = @JoinColumn(name = "preference_id"),
+            inverseJoinColumns = @JoinColumn(name = "customer_id")
+    )
+    private Set<Customer> customers;
 }

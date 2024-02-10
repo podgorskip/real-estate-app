87d02bacc5361440b463e3b6f3a58d85823d49b6
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 18:37:12 +0100

    Adds DTO for returning responses

--- src/main/java/app/estateagency/dto/response/Response.java
+++ src/main/java/app/estateagency/dto/response/Response.java
@@ -1,2 +1,11 @@
-package app.estateagency.dto.response;public class Response {
+package app.estateagency.dto.response;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class Response {
+    private boolean success;
+    private HttpStatus status;
+    private String response;
 }

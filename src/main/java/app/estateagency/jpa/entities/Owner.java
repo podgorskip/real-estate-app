56e4c584b7ae476185128fb854cf46cb76d5e09c
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 15:56:51 +0100

    Adds entity representing owner user

--- src/main/java/app/estateagency/jpa/entities/Owner.java
+++ src/main/java/app/estateagency/jpa/entities/Owner.java
@@ -1,2 +1,24 @@
-package app.estateagency.jpa.entities;public class Owner {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Table(name = "owner", schema = "real_estate")
+@Data
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private Set<Estate> estates;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private Set<OwnerMeeting> meetings;
 }

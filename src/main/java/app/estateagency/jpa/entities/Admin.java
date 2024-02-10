a56ccdbf49844f02e21f15095c3bc5443094631c
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 15:56:20 +0100

    Adds entity representing admin user

--- src/main/java/app/estateagency/jpa/entities/Admin.java
+++ src/main/java/app/estateagency/jpa/entities/Admin.java
@@ -1,2 +1,17 @@
-package app.estateagency.jpa.entities;public class Admin {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "admin", schema = "real_estate")
+@Data
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
 }

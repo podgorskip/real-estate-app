d168a84f1d2d88e54bf3a7baca3d10c930c067de
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 16:16:14 +0100

    Adds entity representing estate history

--- src/main/java/app/estateagency/jpa/entities/HistoryOffer.java
+++ src/main/java/app/estateagency/jpa/entities/HistoryOffer.java
@@ -1,2 +1,32 @@
-package app.estateagency.jpa.entities;public class HistoryOffer {
+package app.estateagency.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "offer_history", schema = "real_estate")
+@Data
+public class HistoryOffer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "estate_id")
+    private Estate estate;
+
+    private double price;
+
+    private String description;
+
+    private LocalDateTime archiveDate;
+
+    @OneToOne(mappedBy = "historyOffer", cascade = CascadeType.ALL)
+    private CustomerReview customerReview;
+
+    @PrePersist
+    private void prePersist() {
+        archiveDate = LocalDateTime.now();
+    }
 }

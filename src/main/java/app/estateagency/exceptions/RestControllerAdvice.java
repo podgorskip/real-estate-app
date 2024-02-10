f1c2bec3e22117cb7c54b5e91e72b4aa8438e1b2
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-10 18:37:35 +0100

    Adds class that handles controllers errors

--- src/main/java/app/estateagency/exceptions/RestControllerAdvice.java
+++ src/main/java/app/estateagency/exceptions/RestControllerAdvice.java
@@ -1,2 +1,29 @@
-package app.estateagency.exceptions;public class ControllerAdvice {
+package app.estateagency.exceptions;
+
+import app.estateagency.dto.response.Response;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class RestControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<Response> handleInvalidArgumentException(MethodArgumentNotValidException e) {
+        Response response = new Response();
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        String message = bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(", "));
+
+        response.setSuccess(false);
+        response.setStatus(HttpStatus.BAD_REQUEST);
+        response.setResponse(message);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }

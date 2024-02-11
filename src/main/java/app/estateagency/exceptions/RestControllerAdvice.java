4b679ac9fb2f828c48db027e333b6cd3439eb90d
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:44:06 +0100

    Adds handler for AuthenticationException

--- src/main/java/app/estateagency/exceptions/RestControllerAdvice.java
+++ src/main/java/app/estateagency/exceptions/RestControllerAdvice.java
@@ -1,6 +1,8 @@
 package app.estateagency.exceptions;
 
 import app.estateagency.dto.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +13,34 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestControllerAdvice {
+    private final Logger log = LogManager.getLogger(RestControllerAdvice.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
     public ResponseEntity<Response> handleInvalidArgumentException(MethodArgumentNotValidException e) {
-        Response response = new Response();
-
         BindingResult bindingResult = e.getBindingResult();
 
         String message = bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(", "));
+        log.error(message);
 
-        response.setSuccess(false);
-        response.setStatus(HttpStatus.BAD_REQUEST);
-        response.setResponse(message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response(false, HttpStatus.BAD_REQUEST, message));
+    }
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseBody
+    public ResponseEntity<Response> handleAuthenticationException(AuthenticationException e) {
+        if (e.getMessage().equals("Invalid token")) {
+
+            String message = "Invalid token provided";
+            log.error(message);
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new Response(false, HttpStatus.UNAUTHORIZED, message));
+        } else {
+
+            String message = "Failed to authenticate user";
+            log.error(message);
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Response(false, HttpStatus.FORBIDDEN, message));
+        }
     }
 }

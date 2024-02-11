f97e3649251919cf8ed0710208a43f64e0c75f4d
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:42:43 +0100

    Adds no args constructor

--- src/main/java/app/estateagency/dto/response/Response.java
+++ src/main/java/app/estateagency/dto/response/Response.java
@@ -1,2 +1,15 @@
-package app.estateagency.dto.response;public class Response {
+package app.estateagency.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+    private boolean success;
+    private HttpStatus status;
+    private String response;
 }

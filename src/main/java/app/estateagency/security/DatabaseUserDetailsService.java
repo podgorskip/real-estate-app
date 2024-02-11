4f1a71c6d5d9c2f3c73385970a404e0a5485e385
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:45:23 +0100

    Adds service loading authenticated user

--- src/main/java/app/estateagency/security/DatabaseUserDetailsService.java
+++ src/main/java/app/estateagency/security/DatabaseUserDetailsService.java
@@ -1,2 +1,27 @@
-package app.estateagency.security;public class DatabaseUserService {
+package app.estateagency.security;
+
+import app.estateagency.jpa.entities.User;
+import app.estateagency.jpa.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DatabaseUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public DatabaseUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty())
+            throw new UsernameNotFoundException("No user of the specified username found");
+
+        return new DatabaseUserDetails(user.get());
+    }
 }

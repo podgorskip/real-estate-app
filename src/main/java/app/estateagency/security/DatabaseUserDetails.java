c2294575ecfc5758b642e4dc35810181e72ba4d0
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:45:01 +0100

    Adds class used for user authentication

--- src/main/java/app/estateagency/security/DatabaseUserDetails.java
+++ src/main/java/app/estateagency/security/DatabaseUserDetails.java
@@ -1,45 +1,50 @@
 package app.estateagency.security;
 
+import app.estateagency.enums.Privilege;
 import app.estateagency.jpa.entities.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 
-public class CustomUserDetails implements UserDetails {
-    private User user;
+public class DatabaseUserDetails implements UserDetails {
+    private final User user;
+
+    public DatabaseUserDetails(User user) {
+        this.user = user;
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return user.getRole().privileges().stream().map(Privilege::name).map(SimpleGrantedAuthority::new).toList();
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }

780f3b015cee27771b2c1bfda446bf233575a99c
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:46:19 +0100

    Adds security filter handling jwt tokens

--- src/main/java/app/estateagency/security/JwtFilter.java
+++ src/main/java/app/estateagency/security/JwtFilter.java
@@ -1,2 +1,48 @@
-package app.estateagency.security;public class JwtFilter {
+package app.estateagency.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.Objects;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+    private final DatabaseUserDetailsService databaseUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String header = request.getHeader(AUTHORIZATION);
+
+        if (Objects.isNull(header) || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+        String username = jwtUtils.extractUsername(token);
+
+        if (Objects.nonNull(username) && Objects.isNull(SecurityContextHolder.getContext().getAuthentication())) {
+            DatabaseUserDetails databaseUserDetails = databaseUserDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(databaseUserDetails, null, databaseUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }

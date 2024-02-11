f80dd7fc923086cea25101bff727587881c57145
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:47:24 +0100

    Adds response when user is authenticated

--- src/main/java/app/estateagency/dto/response/AuthenticationResponse.java
+++ src/main/java/app/estateagency/dto/response/AuthenticationResponse.java
@@ -1,2 +1,13 @@
-package app.estateagency.dto.response;public class AuthenticationResponse {
+package app.estateagency.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String username;
+    private String token;
 }

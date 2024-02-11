e47b9b6f8ea5d7ed14413ed066b8bd0ada3a28d0
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-11 11:42:12 +0100

    Adds DTO for authentication requests

--- src/main/java/app/estateagency/dto/request/AuthenticationRequest.java
+++ src/main/java/app/estateagency/dto/request/AuthenticationRequest.java
@@ -1,2 +1,13 @@
-package app.estateagency.dto.request;public class AuthenticationRequest {
+package app.estateagency.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class AuthenticationRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 }

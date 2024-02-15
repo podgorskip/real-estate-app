ab159fcd4a456a9739902fb5df80836472402382
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 13:25:39 +0100

    Adds method retrieving estates based on predicates

--- src/main/java/app/estateagency/jpa/repositories/EstateRepository.java
+++ src/main/java/app/estateagency/jpa/repositories/EstateRepository.java
@@ -2,22 +2,42 @@ package app.estateagency.jpa.repositories;
 
 import app.estateagency.jpa.entities.Agent;
 import app.estateagency.jpa.entities.Estate;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository used to handles database logic related to Estate entity
+ */
 @Repository
 public interface EstateRepository extends JpaRepository<Estate, Long> {
+    /**
+     * Retrieves all estates assigned to the agent
+     * @param agent Agent to whom estates are assigned
+     * @return List of estates if are present, empty otherwise
+     */
     Optional<List<Estate>> findByAgent(Agent agent);
 
+    /**
+     * Retrieves all the reported estates to which documents and photos are attached
+     * @param agent Agent to whom estates are assigned
+     * @return List of estates if are present, empty otherwise
+     */
     @Query("SELECT e FROM Estate e " +
             "JOIN Document d ON d.estate = e " +
             "JOIN Photo p ON p.estate = e " +
             "LEFT JOIN Offer o ON o.estate = e " +
             "WHERE o IS NULL")
     Optional<List<Estate>> findReportedEstatesByAgent(@Param("agent") Agent agent);
+
+    /**
+     * Retrieves all the estates that match specified predicates
+     * @param specification Specification containing predicates
+     * @return List of estates if are present, empty otherwise
+     */
+    Optional<List<Estate>> findAll(Specification<Estate> specification);
 }

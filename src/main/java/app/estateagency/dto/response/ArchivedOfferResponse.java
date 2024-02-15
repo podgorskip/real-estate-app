7159b504b1a7e7ceeb017a3619cb03cd878ca8ae
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 22:23:26 +0100

    Adds DTO representing archived offer

--- src/main/java/app/estateagency/dto/response/ArchivedOfferResponse.java
+++ src/main/java/app/estateagency/dto/response/ArchivedOfferResponse.java
@@ -1,2 +1,31 @@
-package app.estateagency.dto.response;public class ArchiveOfferResponse {
+package app.estateagency.dto.response;
+
+import app.estateagency.enums.estate.Availability;
+import app.estateagency.enums.estate.Condition;
+import app.estateagency.enums.estate.EstateType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ArchivedOfferResponse {
+    private Long agentID;
+    private String agent;
+    private Long ownerID;
+    private String owner;
+    private Long customerID;
+    private String customer;
+    private EstateType type;
+    private Integer bathrooms;
+    private Integer rooms;
+    private Boolean garage;
+    private Integer storey;
+    private String location;
+    private Boolean balcony;
+    private String description;
+    private Availability availability;
+    private Double size;
+    private Condition condition;
+    private Double price;
+    private LocalDateTime archiveDate;
 }

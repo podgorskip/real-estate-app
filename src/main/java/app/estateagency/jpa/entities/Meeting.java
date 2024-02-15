9da0309a14ff9946f4263720aea140b3ca29881b
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 11:35:45 +0100

    Replaces two separate meeting classes with one

--- src/main/java/app/estateagency/jpa/entities/Meeting.java
+++ src/main/java/app/estateagency/jpa/entities/Meeting.java
@@ -1,2 +1,29 @@
-package app.estateagency.jpa.entities;public class Meeting {
+package app.estateagency.jpa.entities;
+
+import app.estateagency.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "meeting", schema = "real_estate")
+@Data
+public class Meeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
+
+    private LocalDateTime date;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
 }

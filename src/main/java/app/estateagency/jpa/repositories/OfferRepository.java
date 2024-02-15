0d7628b1deb3b771e6e727ea3b1a701110813c95
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 22:21:21 +0100

    Adds methods for fetching blocked offers

--- src/main/java/app/estateagency/jpa/repositories/OfferRepository.java
+++ src/main/java/app/estateagency/jpa/repositories/OfferRepository.java
@@ -3,11 +3,42 @@ package app.estateagency.jpa.repositories;
 import app.estateagency.jpa.entities.Estate;
 import app.estateagency.jpa.entities.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.Optional;
 
+/**
+ * Handles database operations related to Offer entities
+ */
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+    /**
+     * Retrieves the offer by the specified estate
+     * @param estate Estate which is included in the offer
+     * @return Offer object
+     */
     Offer findByEstate(Estate estate);
+
+    /**
+     * Retrieves blocked offers by assigned agent
+     * @param username Username of the agent assigned to the offer
+     * @return List of blocked offers if present, empty otherwise
+     */
+    @Query("SELECT o FROM Offer o " +
+            "WHERE o.blocked IS TRUE " +
+            "AND o.estate.agent.user.username = :username")
+    Optional<List<Offer>> findBlockedOffersByAgentUsername(@Param("username") String username);
+
+    /**
+     * Retrieves blocked offers by the specified customer
+     * @param username Username of the customer whose blocked offers are retrieved
+     * @return List of blocked offers if present, empty otherwise
+     */
+    @Query("SELECT o FROM Offer o " +
+            "WHERE o.blocked IS TRUE " +
+            "AND o.blockedBy.user.username = :username")
+    Optional<List<Offer>> findBlockedOffersByCustomerUsername(@Param("username") String username);
 }

890bf69fd6f8ee367bc83608570cb4260f89a817
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 11:47:52 +0100

    Adds endpoint for customer registration

--- src/main/java/app/estateagency/controllers/CustomerController.java
+++ src/main/java/app/estateagency/controllers/CustomerController.java
@@ -11,12 +11,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  A controller handling requests regarding Customer entities
+ */
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
 
+    /**
+     * Allows to register as a customer
+     * @param userRequest Details of the user who created the account
+     * @return Response if successfully created the account
+     */
     @PostMapping("/auth/customer/register")
     public ResponseEntity<Response> registerCustomer(@Valid @RequestBody UserRequest userRequest) {
         Response response = customerService.createCustomerAccount(userRequest);

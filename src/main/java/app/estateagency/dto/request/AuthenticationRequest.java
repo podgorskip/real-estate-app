d3f1aed99d90b92fc0be89e1bf2047047cf3d371
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 12:03:44 +0100

    Adds attributes validation

--- src/main/java/app/estateagency/dto/request/AuthenticationRequest.java
+++ src/main/java/app/estateagency/dto/request/AuthenticationRequest.java
@@ -1,13 +1,16 @@
 package app.estateagency.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AuthenticationRequest {
-    @NotBlank
+    @NotNull(message = "Username cannot be null")
+    @NotBlank(message = "Username cannot be blank")
     private String username;
 
-    @NotBlank
+    @NotNull(message = "Password cannot be null")
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 }

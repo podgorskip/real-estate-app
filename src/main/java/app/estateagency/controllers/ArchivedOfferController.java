47524065e848e5c08874c238925aab2a64d57a07
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 23:01:29 +0100

    Adds endpoint handling archived offers

--- src/main/java/app/estateagency/controllers/ArchivedOfferController.java
+++ src/main/java/app/estateagency/controllers/ArchivedOfferController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +21,7 @@ import java.util.Optional;
  *  A controller handling requests regarding ArchivedOffer entity
  */
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ArchivedOfferController {
     private final ArchivedOfferService archivedOfferService;

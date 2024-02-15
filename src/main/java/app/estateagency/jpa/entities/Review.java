a0321e9ea631724fc22e2192b670a25ad3ec4f99
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 17:06:24 +0100

    Adds merger review entity

--- src/main/java/app/estateagency/jpa/entities/Review.java
+++ src/main/java/app/estateagency/jpa/entities/Review.java
@@ -1,2 +1,42 @@
-package app.estateagency.jpa.entities;public class Review {
+package app.estateagency.jpa.entities;
+
+import app.estateagency.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "review", schema = "real_estate")
+@Data
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
+
+    @OneToOne
+    @JoinColumn(name = "offer_id")
+    private HistoryOffer historyOffer;
+
+    private int rating;
+
+    private String description;
+
+    private LocalDateTime postDate;
+
+    @PrePersist
+    private void prePersist() {
+        postDate = LocalDateTime.now();
+    }
 }

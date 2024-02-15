525c36ec36671368f28f1b5323cc90a329c5add9
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 16:49:39 +0100

    Adds DTO representing estates

--- src/main/java/app/estateagency/dto/response/OfferResponse.java
+++ src/main/java/app/estateagency/dto/response/OfferResponse.java
@@ -1,2 +1,24 @@
-package app.estateagency.dto.response;public class OfferResponse {
+package app.estateagency.dto.response;
+
+import app.estateagency.enums.estate.Availability;
+import app.estateagency.enums.estate.Condition;
+import app.estateagency.enums.estate.EstateType;
+import lombok.Data;
+
+@Data
+public class OfferResponse {
+    private Long agentID;
+    private String agent;
+    private EstateType type;
+    private Integer bathrooms;
+    private Integer rooms;
+    private Boolean garage;
+    private Integer storey;
+    private String location;
+    private Boolean balcony;
+    private String description;
+    private Availability availability;
+    private Double size;
+    private Condition condition;
+    private Double price;
 }

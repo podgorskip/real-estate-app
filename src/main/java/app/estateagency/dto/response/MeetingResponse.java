96cfb76c40a7ddce960423170e975f8632892258
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 11:34:57 +0100

    Adds DTO representing meeting

--- src/main/java/app/estateagency/dto/response/MeetingResponse.java
+++ src/main/java/app/estateagency/dto/response/MeetingResponse.java
@@ -1,2 +1,13 @@
-package app.estateagency.dto.response;public class MeetingResponse {
+package app.estateagency.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class MeetingResponse {
+    private Long id;
+    private String agent;
+    private String user;
+    private String role;
+    private LocalDateTime date;
 }

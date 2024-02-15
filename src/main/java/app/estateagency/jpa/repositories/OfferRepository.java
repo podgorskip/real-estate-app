f34c5c748e5c3333d82826345673b8a162ede7e3
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 16:50:03 +0100

    Adds method for fetching filtered offers

--- src/main/java/app/estateagency/jpa/repositories/OfferRepository.java
+++ src/main/java/app/estateagency/jpa/repositories/OfferRepository.java
@@ -1,9 +1,13 @@
 package app.estateagency.jpa.repositories;
 
+import app.estateagency.jpa.entities.Estate;
 import app.estateagency.jpa.entities.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+    Offer findByEstate(Estate estate);
 }

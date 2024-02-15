90c672f681f7afa5f39b6665190a79bf63a235e8
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 22:18:04 +0100

    Adds DTO for reviewing offers

--- src/main/java/app/estateagency/dto/request/ReviewRequest.java
+++ src/main/java/app/estateagency/dto/request/ReviewRequest.java
@@ -1,2 +1,19 @@
-package app.estateagency.dto.request;public class ReviewRequest {
+package app.estateagency.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ReviewRequest {
+    @NotNull(message = "Rating cannot be null")
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 10, message = "Rating must be max 10")
+    private Integer rating;
+
+    @NotNull(message = "Comment cannot be null")
+    @NotBlank(message = "Comment cannot be blank")
+    private String comment;
 }

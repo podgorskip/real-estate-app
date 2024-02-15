873680b854ca0c2b7c8f9402919fe8b60e6339c1
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 22:21:49 +0100

    Adds repository for fetching reviews

--- src/main/java/app/estateagency/jpa/repositories/ReviewRepository.java
+++ src/main/java/app/estateagency/jpa/repositories/ReviewRepository.java
@@ -1,2 +1,12 @@
-package app.estateagency.jpa.repositories;public interface ReviewRepository {
+package app.estateagency.jpa.repositories;
+
+import app.estateagency.jpa.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Handles database operations related to Review entities
+ */
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }

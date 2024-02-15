dd09d07e8be7e079583a62171b7a84055a782cc7
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-15 16:48:32 +0100

    Removes endpoint for retrieving filtered estates

--- src/main/java/app/estateagency/controllers/EstateController.java
+++ src/main/java/app/estateagency/controllers/EstateController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,40 +54,6 @@ public class EstateController {
                 .map(estateList -> ResponseEntity
                         .status(HttpStatus.OK)
                         .body(estateList.stream().map(Mapper.INSTANCE::convertEstate).toList()))
-                .orElseGet(() -> ResponseEntity
-                        .status(HttpStatus.NOT_FOUND)
-                        .build());
-    }
-
-    /**
-     * Allows users to check available estates with filters set
-     * @return Response containing list of estates if present
-     */
-    @GetMapping("/auth/estates")
-    public ResponseEntity<List<EstateResponse>> filterEstates(
-            @RequestParam(value = "type", required = false) String type,
-            @RequestParam(value = "bathrooms", required = false) Integer bathrooms,
-            @RequestParam(value = "rooms", required = false) Integer rooms,
-            @RequestParam(value = "garage", required = false) Boolean garage,
-            @RequestParam(value = "storey", required = false) Integer storey,
-            @RequestParam(value = "location", required = false) String location,
-            @RequestParam(value = "balcony", required = false) Boolean balcony,
-            @RequestParam(value = "availability", required = false) String availability,
-            @RequestParam(value = "size", required = false) Double size,
-            @RequestParam(value = "condition", required = false) String condition,
-            @RequestParam(value = "priceFrom", required = false) Double priceFrom,
-            @RequestParam(value = "priceTo", required = false) Double priceTo,
-            @RequestParam(value = "postFrom", required = false) LocalDateTime postFrom,
-            @RequestParam(value = "postTo", required = false) LocalDateTime postTo
-    ) {
-        Optional<List<Estate>> optionalEstates = estateService.getFilteredEstates(
-                bathrooms, rooms, garage, storey, location, balcony, size,
-                condition, type, availability, priceFrom, priceTo, postFrom, postTo);
-
-        return optionalEstates
-                .map(estates -> ResponseEntity
-                        .status(HttpStatus.OK)
-                        .body(estates.stream().map(Mapper.INSTANCE::convertEstate).toList()))
                 .orElseGet(() -> ResponseEntity
                         .status(HttpStatus.NOT_FOUND)
                         .build());

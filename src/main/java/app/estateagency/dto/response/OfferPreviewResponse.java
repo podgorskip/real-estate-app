eea8c7c52764bf875d7eeb9c5757911f72a1f142
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-17 20:23:56 +0100

    Adds DTO for offer preview available without loggin in

--- src/main/java/app/estateagency/dto/response/OfferPreviewResponse.java
+++ src/main/java/app/estateagency/dto/response/OfferPreviewResponse.java
@@ -1,2 +1,13 @@
-package app.estateagency.dto.response;public class OfferPreviewResponse {
+package app.estateagency.dto.response;
+
+import app.estateagency.enums.estate.Availability;
+import lombok.Data;
+
+@Data
+public class OfferPreviewResponse {
+    private Long id;
+    private String location;
+    private Double price;
+    private String description;
+    private Availability availability;
 }

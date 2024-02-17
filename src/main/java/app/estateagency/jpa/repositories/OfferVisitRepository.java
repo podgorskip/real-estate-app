a2509ba0d9e9104ed6124e4bba78eec0dba55d7f
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-17 20:26:06 +0100

    Adds repository for fetching offer visits

--- src/main/java/app/estateagency/jpa/repositories/OfferVisitRepository.java
+++ src/main/java/app/estateagency/jpa/repositories/OfferVisitRepository.java
@@ -1,2 +1,12 @@
-package app.estateagency.jpa.repositories;public class OfferVisitRepository {
+package app.estateagency.jpa.repositories;
+
+import app.estateagency.jpa.entities.OfferVisit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Handles database operations related to OfferVisit entities
+ */
+@Repository
+public interface OfferVisitRepository extends JpaRepository<OfferVisit, Long> {
 }

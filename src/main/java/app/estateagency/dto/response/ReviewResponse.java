d1129c76261f7572c3006de30252882974e5e44c
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-17 19:28:43 +0100

    Adds DTO for reviews

--- src/main/java/app/estateagency/dto/response/ReviewResponse.java
+++ src/main/java/app/estateagency/dto/response/ReviewResponse.java
@@ -1,2 +1,17 @@
-package app.estateagency.dto.response;public class ReviewResponse {
+package app.estateagency.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ReviewResponse {
+    private Long id;
+    private Long agentID;
+    private String agent;
+    private Long reviewerID;
+    private String reviewer;
+    private Long archivedOfferID;
+    private int rating;
+    private String comment;
+    private LocalDateTime postDate;
 }
